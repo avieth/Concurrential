564837781e11b5417f794907c6d839975f7c9bfb
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-04-07 20:39:50 -0400

    Generalize runConcurrential to use arbitrary monad
    I have attempted to define transformers ExceptionSafeT and SharedStateT
    to be used over top of Concurrential, but had a lot of trouble combining
    them, since the ExceptionSafeT needs access to the raw IO, raw IO which
    SharedStateT instances cannot provide.
    
    So, I've taken a step back, and attempted to build extensibility into
    runConcurrential(K)
    
    This is very bizarre and as far as I know unprecedented. Let's see how
    it goes!

--- Control/Concurrent/Concurrential.hs
+++ Control/Concurrent/Concurrential.hs
@@ -17,6 +17,9 @@ would have been run had they been typical IOs.
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Control.Concurrent.Concurrential (
 
@@ -27,12 +30,16 @@ module Control.Concurrent.Concurrential (
   , sequentially
   , concurrently
 
+  , runSimple
+  , runExceptionSafe
+
   ) where
 
 import Control.Applicative
 import Control.Monad
 import Control.Concurrent.Async hiding (concurrently)
 import Control.Exception
+import Data.Functor.Identity
 import Data.Typeable
 
 -- | Description of the way in which an IO should be carried out.
@@ -70,46 +77,60 @@ instance Monad Concurrential where
 --   it allows us to explot @withAsync@, giving us a guarantee that an
 --   exception in a spawning thread will kill spawned threads.
 runConcurrentialK
-  :: Concurrential t
+  :: (Functor m, Applicative m, Monad m)
+  => Concurrential t
   -- ^ The computation to run.
-  -> Async s
+  -> Async (m s)
   -- ^ The sequential part.
-  -> (forall s . (Async s, Async t) -> IO r)
+  -> (forall a . IO a -> IO (m a))
+  -- ^ For extensibility
+  -> (forall a . m (IO (m a)) -> IO (m a))
+  -- ^ For extensibility
+  -> (forall s . (Async (m s), Async (m t)) -> IO (m r))
   -- ^ The continuation; fst is sequential part, snd is value part.
   --   We use the rank 2 type for s because we really don't care what the
   --   value of the sequential part it, we just need to wait for it and then
   --   continue with >>.
-  -> IO r
-runConcurrentialK sc sequentialPart k = case sc of
+  -> IO (m r)
+runConcurrentialK sc sequentialPart extender retractor k = case sc of
     SCAtom choice -> case choice of
         -- The async created becomes the sequential part and the value
         -- part. So when another Sequential is encountered, its value part
         -- will have to wait for this computation to complete.
-        Sequential io -> withAsync (wait sequentialPart >> io) (\async -> k (async, async))
+        Sequential io -> withAsync
+                         (wait sequentialPart >> extender io)
+                         (\async -> k (async, async))
         -- The async created is the value part, but the sequential part
         -- remains the same.
-        Concurrent io -> withAsync io (\async -> k (sequentialPart, async))
-    SCBind sc next -> runConcurrentialK sc sequentialPart $ \(sequentialPart, asyncS) -> do
+        Concurrent io -> withAsync
+                         (extender io)
+                         (\async -> k (sequentialPart, async))
+    SCBind sc next -> runConcurrentialK sc sequentialPart extender retractor $ \(sequentialPart, asyncS) -> do
         s <- wait asyncS
-        runConcurrentialK (next s) sequentialPart k
+        retractor (fmap (\x -> runConcurrentialK (next x) sequentialPart extender retractor k) s)
     SCAp left right ->
-        runConcurrentialK left sequentialPart $ \(sequentialPart, asyncF) ->
-        runConcurrentialK right sequentialPart $ \(sequentialPart, asyncX) ->
+        runConcurrentialK left sequentialPart extender retractor $ \(sequentialPart, asyncF) ->
+        runConcurrentialK right sequentialPart extender retractor $ \(sequentialPart, asyncX) ->
         let waitAndApply = do
               f <- wait asyncF
               x <- wait asyncX
-              return $ f x
-        in withAsync waitAndApply (\async -> k (sequentialPart, async))
+              return $ f <*> x
+        in  withAsync waitAndApply (\async -> k (sequentialPart, async))
 
 -- | Run a Concurrential term, realizing the effects of the IOs which compose
 --   it.
-runConcurrential :: Concurrential t -> IO t
-runConcurrential c = do
+runConcurrential
+  :: (Functor m, Applicative m, Monad m)
+  => (forall a . IO a -> IO (m a))
+  -> (forall a . m (IO (m a)) -> IO (m a))
+  -> Concurrential t
+  -> IO (m t)
+runConcurrential extender retractor c = do
     -- I believe it is safe to supply the async in this way, without using
     -- withAsync, because the computation is trivial, and we need not worry
     -- about this thread dangling.
-    sequentialPart <- async $ return ()
-    runConcurrentialK c sequentialPart (wait . snd)
+    sequentialPart <- async $ return (return ())
+    runConcurrentialK c sequentialPart extender retractor (wait . snd)
 
 -- | Create an IO which must be run sequentially.
 --   If a @sequentially io@ appears in a @Concurrential t@ term then it will
@@ -146,3 +167,17 @@ sequentially = SCAtom . Sequential
 --   been used.
 concurrently :: IO t -> Concurrential t
 concurrently = SCAtom . Concurrent
+
+runSimple :: Concurrential t -> IO (Identity t)
+runSimple = runConcurrential extender retractor
+  where
+    extender = fmap Identity
+    retractor = runIdentity
+
+runExceptionSafe :: Concurrential t -> IO (Either SomeException t)
+runExceptionSafe = runConcurrential extender retractor
+  where
+    extender io = (Right <$> io) `catch` (\(e :: SomeException) -> return $ Left e)
+    retractor x = case x of
+        Left e -> return $ Left e
+        Right io -> io
